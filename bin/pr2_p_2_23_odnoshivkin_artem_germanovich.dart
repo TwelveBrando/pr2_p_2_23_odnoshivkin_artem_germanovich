ba659630f0e56a13e7d6e1da46be4ceea9cc8ed2
Twelve <limelaimov4567@gmail.com>
2025-02-27 12:58:54 +0300

    added

--- bin/pr2_p_2_23_odnoshivkin_artem_germanovich.dart
+++ bin/pr2_p_2_23_odnoshivkin_artem_germanovich.dart
@@ -2,11 +2,11 @@ import 'dart:io';
 
 void main() {
   stdout.write('Введите название книги: ');
-  String? nameBook = stdin.readLineSync(); // null может быть - ?
+  String? nameBook = stdin.readLineSync();
   stdout.write('Введите автора книги: ');
-  String? createrBook = stdin.readLineSync(); // null может быть - ?
+  String? createrBook = stdin.readLineSync();
   stdout.write('Введите год издания книги: ');
-  String? dateBook = stdin.readLineSync(); // null может быть - ?
+  String? dateBook = stdin.readLineSync();
   File file = File('books.txt');
   final books = Directory('books');
   if (file.existsSync()) {
@@ -14,14 +14,14 @@ void main() {
     file.createSync();
   }
   file.writeAsStringSync(
-      "Название: ${nameBook}, Автор: ${dateBook}, Дата издания: ${dateBook}.\n",
+      "Название: ${nameBook}, Автор: ${createrBook}, Дата издания: ${dateBook}.\n",
       mode: FileMode.append);
   if (books.existsSync()) {
   } else {
     Directory('books').createSync();
   }
   var stat = file.statSync();
-
-  file.renameSync(
-      "./books/${stat.changed.toString().replaceAll(' ', '').replaceAll(':', '-')}.txt");
+  file.copySync(
+      "./books/${stat.changed.toString().replaceAll(' ', '').replaceAll(':', '-').substring(0, 10)}.txt");
+  file.deleteSync();
 }
